93dfdeceb727c771e901e2795de262ded09c9fa3
Egor Polovko <skelletrise@gmail.com>
2025-03-11 17:11:26 +0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,20 +4,20 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="flex justify-center w-full bg-background text-text min-h-15">
-      <div className="flex justify-between items-center px-8 py-2 mx-auto w-full">
+      <div className="flex items-center justify-between w-full px-8 py-2 mx-auto">
         <div className="flex gap-4 text-xl font-bold">
-          <div>BS information</div>
+          <div>Информация по БС</div>
           <ToggleTheme />
         </div>
         <nav className="flex space-x-4">
           <Link to={"/"} className="transition-all hover:text-blue-300">
-            BS Info
+            БС Инфо
           </Link>
           <Link
             to={"/bs-voltage"}
             className="transition-all hover:text-blue-300"
           >
-            BS Voltage Info
+            Напряжение на БС
           </Link>
         </nav>
       </div>
