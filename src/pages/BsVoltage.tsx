e31c83774a837540ceb84ad3c4117fb7383432a6
Egor Polovko <skelletrise@gmail.com>
2025-03-12 17:51:23 +0700

    Update BsVoltage.tsx

--- src/pages/BsVoltage.tsx
+++ src/pages/BsVoltage.tsx
@@ -1,10 +1,10 @@
 const BsVoltage = () => {
   return ( 
-    <div className="h-screen flex items-center justify-center">
-      <h1 className="text-3xl text-text>
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl text-text">
         Тут еще будет страница
       </h1>
-    </div>;
+    </div>
     )
 };
 
