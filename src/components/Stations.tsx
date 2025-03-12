da3d9e275e8284d9dd062196f2ad94d8268e20bd
Egor Polovko <skelletrise@gmail.com>
2025-03-12 17:26:13 +0700

    Update Stations.tsx

--- src/components/Stations.tsx
+++ src/components/Stations.tsx
@@ -5,29 +5,31 @@ const Stations = () => {
   const [searchTerm, setSearchTerm] = useState<string>("");
 
   return (
-    <aside className="min-w-[350px] bg-blue-50 rounded-lg shadow-lg p-4 overflow-y-scroll scrollbar-none pb-12">
-      <div>
-        {/* Поле ввода */}
-        <div className="mb-6">
-          <input
-            type="text"
-            placeholder="Поиск..."
-            className="w-full px-4 py-2 transition-all duration-200 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-accent focus:border-transparent hover:border-gray-400"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
+    <aside className="pt-15">
+      <div className="min-w-[350px] bg-blue-50 rounded-lg shadow-lg p-4 overflow-y-scroll scrollbar-none pb-12 h-full">
+        <div>
+          {/* Поле ввода */}
+          <div className="mb-6">
+            <input
+              type="text"
+              placeholder="Поиск..."
+              className="w-full px-4 py-2 transition-all duration-200 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-accent focus:border-transparent hover:border-gray-400"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
         </div>
-      </div>
-
-      {/* Заголовки */}
-      <div className="mb-4">
-        <div className="flex items-center justify-between px-4 text-lg font-semibold text-gray-700">
-          <div>База</div>
-          <div>Месяц</div>
-          <div>Год</div>
+  
+        {/* Заголовки */}
+        <div className="mb-4">
+          <div className="flex items-center justify-between px-4 text-lg font-semibold text-gray-700">
+            <div>База</div>
+            <div>Месяц</div>
+            <div>Год</div>
+          </div>
         </div>
+        <StationList searchTerm={searchTerm} />
       </div>
-      <StationList searchTerm={searchTerm} />
     </aside>
   );
 };
