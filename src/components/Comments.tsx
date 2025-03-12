88224ea7858971f4facaea67f2792736c0b26d5c
Egor Polovko <skelletrise@gmail.com>
2025-03-12 17:24:49 +0700

    Update Comments.tsx

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -49,66 +49,68 @@ const Comments: React.FC<Props> = ({ data }) => {
   };
 
   return (
-    <div className="min-w-[350px] max-w-[355px] bg-blue-50 rounded-lg shadow-lg p-4 flex flex-col max-h-screen pb-12">
-      <h2 className="mb-4 text-xl font-semibold text-gray-800">Комментарии</h2>
-
-      {/* Список комментариев */}
-      <div className="flex-1 mb-4 space-y-3 overflow-y-auto">
-        {comments.map((comment, index) => (
-          <div
-            key={index}
-            className="p-3 overflow-hidden break-words rounded-lg shadow-sm bg-background text-text"
-          >
-            <p className="text-sm text-text">{comment.date}</p>
-            <p className="text-text">{comment.comment}</p>
-            <div className="flex justify-between">
-              <p className="text-xs text-text">{comment.type_failure}</p>
-              <p className="text-xs text-text">{comment.status}</p>
+    <div className='pt-15'>
+      <div className="min-w-[350px] max-w-[355px] bg-blue-50 rounded-lg shadow-lg p-4 flex flex-col pb-12 h-full ">
+        <h2 className="mb-4 text-xl font-semibold text-gray-800">Комментарии</h2>
+  
+        {/* Список комментариев */}
+        <div className="flex-1 mb-4 space-y-3 overflow-y-auto">
+          {comments.map((comment, index) => (
+            <div
+              key={index}
+              className="p-3 overflow-hidden break-words rounded-lg shadow-sm bg-background text-text"
+            >
+              <p className="text-sm text-text">{comment.date}</p>
+              <p className="text-text">{comment.comment}</p>
+              <div className="flex justify-between">
+                <p className="text-xs text-text">{comment.type_failure}</p>
+                <p className="text-xs text-text">{comment.status}</p>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
-
-      {/* Поле для ввода нового комментария */}
-      <div className="flex flex-col gap-2">
-        <textarea
-          value={newComment}
-          onChange={(e) => setNewComment(e.target.value)}
-          placeholder="Напишите комментарий..."
-          className="flex-1 p-2 max-w-full rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 resize-y min-h-[120px] dark:bg-gray-700 dark:text-gray-200"
-          rows={1}
-        />
-
-        {/* Выбор статуса */}
-        <select
-          value={status || ""}
-          onChange={(e) => setStatus(e.target.value || null)}
-          className="p-2 border border-gray-300 rounded-lg"
-        >
-          <option value="">Выберите статус</option>
-          <option value="решена">Решена</option>
-          <option value="в работе">В работе</option>
-        </select>
-
-        {/* Выбор типа отказа */}
-        <select
-          value={typeFailure || ""}
-          onChange={(e) => setTypeFailure(e.target.value || null)}
-          className="p-2 border border-gray-300 rounded-lg"
-        >
-          <option value="">Выберите тип отказа</option>
-          <option value="АКБ">АКБ</option>
-          <option value="Транспорт">Транспорт</option>
-          <option value="АКБ + транспорт">АКБ + транспорт</option>
-          <option value="Иное">Иное</option>
-        </select>
-
-        <button
-          onClick={handleAddComment}
-          className="px-4 py-2 text-white transition-colors duration-200 bg-blue-500 rounded-lg hover:bg-blue-600"
-        >
-          Отправить
-        </button>
+          ))}
+        </div>
+  
+        {/* Поле для ввода нового комментария */}
+        <div className="flex flex-col gap-2">
+          <textarea
+            value={newComment}
+            onChange={(e) => setNewComment(e.target.value)}
+            placeholder="Напишите комментарий..."
+            className="flex-1 p-2 max-w-full rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 resize-y min-h-[120px] dark:bg-gray-700 dark:text-gray-200"
+            rows={1}
+          />
+  
+          {/* Выбор статуса */}
+          <select
+            value={status || ""}
+            onChange={(e) => setStatus(e.target.value || null)}
+            className="p-2 border border-gray-300 rounded-lg"
+          >
+            <option value="">Выберите статус</option>
+            <option value="решена">Решена</option>
+            <option value="в работе">В работе</option>
+          </select>
+  
+          {/* Выбор типа отказа */}
+          <select
+            value={typeFailure || ""}
+            onChange={(e) => setTypeFailure(e.target.value || null)}
+            className="p-2 border border-gray-300 rounded-lg"
+          >
+            <option value="">Выберите тип отказа</option>
+            <option value="АКБ">АКБ</option>
+            <option value="Транспорт">Транспорт</option>
+            <option value="АКБ + транспорт">АКБ + транспорт</option>
+            <option value="Иное">Иное</option>
+          </select>
+  
+          <button
+            onClick={handleAddComment}
+            className="px-4 py-2 text-white transition-colors duration-200 bg-blue-500 rounded-lg hover:bg-blue-600"
+          >
+            Отправить
+          </button>
+        </div>
       </div>
     </div>
   );
