01c304b07c821d42820ca588d2fdc8da4bae46d3
Egor Polovko <skelletrise@gmail.com>
2025-03-12 17:33:06 +0700

    Update Information.tsx

--- src/components/Information.tsx
+++ src/components/Information.tsx
@@ -26,38 +26,39 @@ const Information: React.FC<InformationProps> = ({onBaseInfoUpdate }) => {
   }
 
   return (
-    <div className="flex-1 min-w-[350px] bg-blue-50 rounded-lg shadow-lg p-6 w-full overflow-y-auto h-full scrollbar-none pb-12">
-      {/* Заголовок */}
-      <div className="flex items-center justify-center mb-6">
-        <h1 className="text-2xl font-semibold text-gray-800">
-          {stationId || "NS1120"}
-        </h1>
-      </div>
-
-      {/* Основной контент */}
-      <div className="flex justify-between gap-6">
-        {/* Блок с информацией */}
-        <div className="flex-1 p-4 overflow-hidden bg-white rounded-lg shadow-md bg-background text-text">
-          <h2 className="mb-4 text-xl font-semibold">Информация</h2>
-          <div className="space-y-3 break-words">
-            <p>Название: {stationId || "NS1120"}</p>
-            <p>Месяц: 99.5%</p>
-            <p>Год: 98.9%</p>
-            <p>Статус: Активна</p>
+      <div className="flex-1 min-w-[350px] bg-blue-50 rounded-lg shadow-lg p-6 w-full overflow-y-auto h-full scrollbar-none pb-12">
+        {/* Заголовок */}
+        <div className="flex items-center justify-center mb-6">
+          <h1 className="text-2xl font-semibold text-gray-800">
+            {stationId || "NS1120"}
+          </h1>
+        </div>
+  
+        {/* Основной контент */}
+        <div className="flex justify-between gap-6">
+          {/* Блок с информацией */}
+          <div className="flex-1 p-4 overflow-hidden bg-white rounded-lg shadow-md bg-background text-text">
+            <h2 className="mb-4 text-xl font-semibold">Информация</h2>
+            <div className="space-y-3 break-words">
+              <p>Название: {stationId || "NS1120"}</p>
+              <p>Месяц: 99.5%</p>
+              <p>Год: 98.9%</p>
+              <p>Статус: Активна</p>
+            </div>
+          </div>
+  
+          {/* График */}
+          <div className="flex items-center justify-center flex-1 p-4 rounded-lg shadow-md bg-background">
+            <Schedule data={data} />
           </div>
         </div>
-
-        {/* График */}
-        <div className="flex items-center justify-center flex-1 p-4 rounded-lg shadow-md bg-background">
-          <Schedule data={data} />
+  
+        {/* Таблица */}
+        <div className="mt-6">
+          <BsTable dataInfo={data} />
         </div>
       </div>
 
-      {/* Таблица */}
-      <div className="mt-6">
-        <BsTable dataInfo={data} />
-      </div>
-    </div>
   );
 };
 
