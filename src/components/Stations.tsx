861eb787966f2b1f818102280505c5b9dcf32a39
Egor Polovko <skelletrise@gmail.com>
2025-03-12 17:45:40 +0700

    Update Stations.tsx

--- src/components/Stations.tsx
+++ src/components/Stations.tsx
@@ -5,7 +5,7 @@ const Stations = () => {
   const [searchTerm, setSearchTerm] = useState<string>("");
 
   return (
-    <aside className="min-w-[350px] bg-blue-50 rounded-lg shadow-lg p-4 overflow-y-scroll scrollbar-none pb-12 h-full">
+    <aside className="min-w-[350px] bg-blue-50 rounded-lg shadow-lg p-4 overflow-y-scroll scrollbar-none pb-6 h-full">
         <div>
           {/* Поле ввода */}
           <div className="mb-6">
