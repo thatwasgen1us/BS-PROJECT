cacbef12e05390f8465d1bd55e6b03784fd2d76b
Egor Polovko <skelletrise@gmail.com>
2025-03-19 11:30:01 +0700

    Update StationList.tsx

--- src/components/StationList.tsx
+++ src/components/StationList.tsx
@@ -1,7 +1,7 @@
 import { Link, useParams } from "react-router-dom";
 import { useGetBaseDataQuery } from "@/api/api";
 import { Error, Spinner } from "@/components";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo, useCallback } from "react";
 
 type Props = {
   searchTerm: string;
@@ -23,35 +23,44 @@ const StationList: React.FC<Props> = ({ searchTerm }) => {
     localStorage.setItem('hideZeroBases', hideZeroBases.toString());
   }, [hideZeroBases]);
 
-  const filteredStations = data
-    ? data.filter((station) => {
-        const isUnique = !uniqueStationNames.has(station.BS_NAME);
-        uniqueStationNames.add(station.BS_NAME);
-        const matchesSearchTerm = station.BS_NAME.toLowerCase().includes(searchTerm.toLowerCase());
-        const isZeroBase = hideZeroBases ? Number(station.CA_52w) !== 0 : true;
-        return isUnique && matchesSearchTerm && isZeroBase;
-      })
-    : [];
+  // Фильтрация станций
+  const filteredStations = useMemo(() => {
+    if (!data) return [];
+    return data.filter((station) => {
+      const isUnique = !uniqueStationNames.has(station.BS_NAME);
+      uniqueStationNames.add(station.BS_NAME);
+      const matchesSearchTerm = station.BS_NAME.toLowerCase().includes(searchTerm.toLowerCase());
+      const isZeroBase = hideZeroBases ? Number(station.CA_52w) !== 0 : true;
+      return isUnique && matchesSearchTerm && isZeroBase;
+    });
+  }, [data, searchTerm, hideZeroBases]);
 
-  const sortedStations = filteredStations.sort((a, b) => {
-    let comparison = 0;
+  // Сортировка станций
+  const sortedStations = useMemo(() => {
+    return [...filteredStations].sort((a, b) => {
+      let comparison = 0;
+      switch (sortCriteria) {
+        case 'name':
+          comparison = a.BS_NAME.localeCompare(b.BS_NAME);
+          break;
+        case 'month':
+          comparison = b.CA_4w - a.CA_4w; 
+          break;
+        case 'year':
+          comparison = b.CA_52w - a.CA_52w; 
+          break;
+        default:
+          return 0;
+      }
+      return sortOrder === 'asc' ? comparison : -comparison;
+    });
+  }, [filteredStations, sortCriteria, sortOrder]);
 
-    switch (sortCriteria) {
-      case 'name':
-        comparison = a.BS_NAME.localeCompare(b.BS_NAME);
-        break;
-      case 'month':
-        comparison = b.CA_4w - a.CA_4w; 
-        break;
-      case 'year':
-        comparison = b.CA_52w - a.CA_52w; 
-        break;
-      default:
-        return 0;
-    }
-
-    return sortOrder === 'asc' ? comparison : -comparison; 
-  });
+  // Обработчики для кнопок сортировки
+  const handleSort = useCallback((criteria: 'name' | 'month' | 'year') => {
+    setSortCriteria(criteria);
+    setSortOrder((prevOrder) => (prevOrder === 'asc' ? 'desc' : 'asc'));
+  }, []);
 
   if (isLoading) {
     return <Spinner />;
@@ -64,57 +73,51 @@ const StationList: React.FC<Props> = ({ searchTerm }) => {
   return (
     <div className="flex flex-col h-screen">
       {/* Фиксированная верхняя часть */}
-      <div className="sticky top-0 z-10 px-4 border-none shadow-sm">
-        <div className="px-2 pt-2">
+      <div className="sticky top-0 z-10 flex flex-col px-6 pr-8 bg-transparent">
+        <div className="py-2">
           <label className="flex items-center space-x-2">
             <input
               type="checkbox"
               checked={hideZeroBases}
               onChange={(e) => setHideZeroBases(e.target.checked)}
-              className="w-5 h-5 text-blue-600 border-gray-300 rounded form-checkbox focus:ring-blue-500"
+              className="w-5 h-5 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
             />
             <span className="text-gray-700">Скрыть базы с 0% за год</span>
           </label>
         </div>
         {/* Элементы управления для сортировки */}
-        <div className="px-2 border-none">
-          <div className="flex justify-between">
-            <button
-              className="cursor-pointer"
-              onClick={() => {
-                setSortCriteria('name');
-                setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc'); 
-              }}>
-              База {sortCriteria === 'name' && (sortOrder === 'asc' ? '↑' : '↓')}
-            </button>
-            <button
-              className="cursor-pointer"
-              onClick={() => {
-                setSortCriteria('month');
-                setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc'); 
-              }}>
-              Месяц {sortCriteria === 'month' && (sortOrder === 'asc' ? '↑' : '↓')}
-            </button>
-            <button
-              className="cursor-pointer" 
-              onClick={() => {
-                setSortCriteria('year');
-                setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc'); 
-              }}>
-              Год {sortCriteria === 'year' && (sortOrder === 'asc' ? '↑' : '↓')}
-            </button>
-          </div>
+        <div className="flex items-center justify-around py-2">
+          <button
+            className="cursor-pointer hover:text-blue-600"
+            onClick={() => handleSort('name')}
+          >
+            База {sortCriteria === 'name' && (sortOrder === 'asc' ? '↑' : '↓')}
+          </button>
+          <button
+            className="cursor-pointer hover:text-blue-600"
+            onClick={() => handleSort('month')}
+          >
+            Месяц {sortCriteria === 'month' && (sortOrder === 'asc' ? '↑' : '↓')}
+          </button>
+          <button
+            className="cursor-pointer hover:text-blue-600"
+            onClick={() => handleSort('year')}
+          >
+            Год {sortCriteria === 'year' && (sortOrder === 'asc' ? '↑' : '↓')}
+          </button>
         </div>
       </div>
 
       {/* Скроллируемая нижняя часть */}
-      <div className="flex flex-col flex-1 px-2 pt-2 overflow-y-auto gap-y-2">
+      <div className="flex-1 px-4 py-2 space-y-2 overflow-y-auto">
         {sortedStations.length > 0 ? (
           sortedStations.map((station, index) => (
             <Link
               to={`/base/${station.BS_NAME}`}
               key={station.BS_NAME + index}
-              className={station.BS_NAME === stationId ? "flex items-center justify-between p-4 transition-all duration-200 rounded-lg shadow-sm bg-accent hover:shadow-md" : "flex items-center justify-between p-4 transition-all duration-200 rounded-lg shadow-sm bg-background hover:shadow-md hover:bg-accent"}
+              className={`flex items-center justify-between p-4 transition-all duration-200 rounded-lg shadow-sm hover:shadow-md ${
+                station.BS_NAME === stationId ? "bg-accent" : "bg-background hover:bg-accent"
+              }`}
             >
               <div className="text-lg font-semibold text-text">
                 {station.BS_NAME}
@@ -124,7 +127,7 @@ const StationList: React.FC<Props> = ({ searchTerm }) => {
             </Link>
           ))
         ) : (
-          <div>No stations found.</div>
+          <div className="text-center text-gray-500">No stations found.</div>
         )}
       </div>
     </div>
